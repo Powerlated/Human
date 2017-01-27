f08fe3d97c7d8c67be33d072e62bb23ad50baafb
Powerlated <zjia08@gmail.com>
2017-01-27 16:00:11 -0500

    Fixed cmmits.

--- src/Human.java
+++ src/Human.java
@@ -1,4 +1,5 @@
 import java.util.HashSet;
+import java.util.Random;
 
 public class Human {
 	private Type type;
@@ -12,6 +13,17 @@ public class Human {
 	}
 	
 	public static void newHuman() {
-		
+		Random r = new Random();
+		int i = r.nextInt(1);
+		if (i == 0) {
+			new Human(Human.Type.FEMALE);
+		} else if (i == 1) {
+			int other = r.nextInt(4);
+			if (other == 0){
+				new Human(Human.Type.OTHER);
+				return;
+			}
+			new Human(Human.Type.MALE);		
+		}
 	}
 }
--- src/Initiate.java
+++ src/Initiate.java
@@ -2,17 +2,6 @@ import java.util.Random;
 
 public class Initiate {
 	public static void main(String[] args) {
-		Random r = new Random();
-		int i = r.nextInt(1);
-		if (i == 0) {
-			new Human(Human.Type.FEMALE);
-		} else if (i == 1) {
-			int other = r.nextInt(4);
-			if (other == 0){
-				new Human(Human.Type.OTHER);
-				return;
-			}
-			new Human(Human.Type.MALE);		
-		}
+		Human.newHuman();
 	}
 }
